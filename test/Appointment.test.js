620d57446e6ce68c97ad64ea155ce9ec016cd720
luna.lee <luna.lee@nextfortune.tw>
2020-09-26 19:25:52 +0800

    :tada: init

---
+++ test/Appointment.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+
+describe('Appointment', () => {
+  it('renders the customer first name', () => {
+    const customer = { name: 'cuteLuna' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(component, container);
+    expect(document.body.textContent).toMatch('cuteLuna');
+  })
+})
