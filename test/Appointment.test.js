44496ab2f2afee081c6f040ef47e969027ec6e87
Luna Goodnight <catsheue@gmail.com>
2020-09-27 17:56:58 +0800

    :white_check_mark: add test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -11,4 +11,12 @@ describe('Appointment', () => {
     ReactDOM.render(component, container);
     expect(document.body.textContent).toMatch('cuteLuna');
   })
+  it.skip('renders another customer first name', () => {
+    const customer = { name: 'miou' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(component, container);
+    expect(document.body.textContent).toMatch('miou');
+  })
 })
